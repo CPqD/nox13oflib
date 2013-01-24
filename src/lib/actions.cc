ebb428489f0f408ecef651e17b0bc540674dd5fe
ederlf <ederlf@cpqd.com.br>
2013-01-24 14:36:13 -0200

    Fix push mpls action

--- src/lib/actions.cc
+++ src/lib/actions.cc
@@ -10,13 +10,13 @@ int Actions::act_num;
 Actions::Actions(){
     act_num = 0;
     acts = (struct ofl_action_header**) xmalloc(sizeof(struct ofl_action_header *));
-
 }
 
 void
 Actions::CreateOutput(uint32_t port, uint16_t max_len){
     
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_output *a = (struct ofl_action_output*) xmalloc(sizeof(struct ofl_action_output));
     a->port = port;
     a->max_len = max_len;
@@ -28,7 +28,8 @@ Actions::CreateOutput(uint32_t port, uint16_t max_len){
 void
 Actions::CreateOutput(uint32_t port){
     
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_output *a = (struct ofl_action_output*) xmalloc(sizeof(struct ofl_action_output));
     a->port = port;
     a->max_len = 0;
@@ -41,10 +42,11 @@ void
 Actions::CreateCopyTTL(enum ofp_action_type type){
 
     if (type != OFPAT_COPY_TTL_IN && type != OFPAT_COPY_TTL_OUT ){
-
+        //#TODO Error message
     }
     else {
-        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+        if (act_num)
+            acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
         acts[act_num]->type = type;
         struct ofl_action_header *a = (struct ofl_action_header*) xmalloc(sizeof(struct ofl_action_header));
         acts[act_num] = (struct ofl_action_header*) a;
@@ -56,10 +58,11 @@ void
 Actions::CreateDecTTL(enum ofp_action_type type){
     
     if (type != OFPAT_DEC_MPLS_TTL && type != OFPAT_DEC_NW_TTL ){
-
+        //#TODO Error message
     }
     else {
-        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+        if (act_num)
+            acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
         struct ofl_action_header *a = (struct ofl_action_header*) xmalloc(sizeof(struct ofl_action_header));
         acts[act_num] = (struct ofl_action_header*) a;
         acts[act_num]->type = type;
@@ -68,23 +71,27 @@ Actions::CreateDecTTL(enum ofp_action_type type){
 }
     
 void 
-Actions::CreatePushAction(enum ofp_action_type type){
+Actions::CreatePushAction(enum ofp_action_type type, uint16_t ethertype){
 
-    if (type != OFPAT_PUSH_MPLS && type != OFPAT_PUSH_VLAN ){
-
+    if (type != OFPAT_PUSH_MPLS && type != OFPAT_PUSH_VLAN && type != OFPAT_PUSH_PBB){
+        //#TODO Error message
     }
     else {
-        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);    
+        if (act_num)
+            acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1));    
+        struct ofl_action_push *psh = (struct ofl_action_push*) xmalloc(sizeof(struct ofl_action_push));
+        psh->ethertype = ethertype;
+        acts[act_num] = (struct ofl_action_header*) psh;
         acts[act_num]->type = type;
-        struct ofl_action_header *a = (struct ofl_action_header*) xmalloc(sizeof(struct ofl_action_header));
-        acts[act_num] = (struct ofl_action_header*) a;
         act_num++;
     }
 }
     
 void 
 Actions::CreatePopVlan(){
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_header *a = (struct ofl_action_header*) xmalloc(sizeof(struct ofl_action_header));
     acts[act_num] = (struct ofl_action_header*) a;
     acts[act_num]->type = OFPAT_POP_VLAN;
@@ -93,7 +100,9 @@ Actions::CreatePopVlan(){
     
 void 
 Actions::CreatePopMpls(uint16_t ethertype){
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_pop_mpls *a = (struct ofl_action_pop_mpls*) xmalloc(sizeof(struct ofl_action_pop_mpls));
     a->ethertype = ethertype;
     acts[act_num] = (struct ofl_action_header*) a;
@@ -103,11 +112,13 @@ Actions::CreatePopMpls(uint16_t ethertype){
     
 void 
 Actions::CreateSetNwTTL(uint8_t nw_ttl){
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_set_nw_ttl *a = (struct ofl_action_set_nw_ttl*) xmalloc(sizeof(struct ofl_action_set_nw_ttl));
     a->nw_ttl = nw_ttl;
     acts[act_num] = (struct ofl_action_header*) a;
-    acts[act_num]->type = OFPAT_SET_NW_TTL;    
+    acts[act_num]->type = OFPAT_SET_NW_TTL;
     act_num++;
 }
 
@@ -123,7 +134,9 @@ Actions::CreateSetMplsTTL(uint8_t mpls_ttl){
     
 void 
 Actions::CreateSetQueue(uint32_t queue_id){
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_set_queue *a = (struct ofl_action_set_queue*) xmalloc(sizeof(struct ofl_action_set_queue));
     a->queue_id = queue_id;
     acts[act_num] = (struct ofl_action_header*) a;
@@ -133,7 +146,9 @@ Actions::CreateSetQueue(uint32_t queue_id){
     
 void 
 Actions::CreateGroupAction(uint32_t group_id){
-    acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * act_num);
+    
+    if (act_num)
+        acts = (struct ofl_action_header**) xrealloc(acts, sizeof(struct ofl_action_header *) * (act_num +1)); 
     struct ofl_action_group *a = (struct ofl_action_group*) xmalloc(sizeof(struct ofl_action_group));
     a->group_id = group_id;
     acts[act_num] = (struct ofl_action_header*) a;
